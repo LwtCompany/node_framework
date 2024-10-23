b8f4cf0f4dc592f315aaf07a572a57a5ce65b186
xurshid soliyev <hunter982019@gmail.com>
2024-10-23 15:02:30 +0500

    middleware added

--- src/framework.ts
+++ src/framework.ts
@@ -1,9 +1,12 @@
 import http, {IncomingMessage,ServerResponse} from 'http'
 import Router from "./router";
+import Middleware from "./midlleware";
 
 
 class Framework {
     private routers:Router[] = [];
+    private middlewares:Middleware[] = [];
+
     public start(port:number, cb: (() => void) | undefined = undefined) {
         const server = http.createServer(this.handleRequest.bind(this));
         server.listen(port, cb);
@@ -11,15 +14,28 @@ class Framework {
     public useRouter(router:Router){
         this.routers.push(router);
     }
+    public useMiddleware(middleware:Middleware){
+        this.middlewares.push(middleware);
+    }
     private handleRequest(req: IncomingMessage, res: ServerResponse){
-        for (const router of this.routers) {
-              if (router.handler(req, res)) {
-                return;
-              }
-            }
+        let i = 0;
+        const next = () => {
+                if(i < this.middlewares.length){
+                this.middlewares[i].runMiddleware(req, res, () => {
+                     for (const router of this.routers) {
+                        if (router.handler(req, res)) {
+                          return; // Stop when a route is handled
+                        }
+                      }
 
-            res.statusCode = 404;
-            res.end('Not Found');
+                      res.statusCode = 404;
+                      res.end('Not Found');
+                })
+                i++;
+            }
+        }
+        next();
+
     }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import Framework from "./framework";
 import Router from "./router";
+import Middleware from "./midlleware";
 const app = new Framework();
 const router = new Router();
+const middleware = new Middleware();
+
+middleware.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+middleware.use((req,res, next) => {
+    console.log(`second middleware`);
+    next()
+})
 router.get('/home', (req, res) => {
     res.statusCode = 200;
     res.end("Welcome to the home");
@@ -11,6 +22,7 @@ router.get('/about', (req, res) => {
 });
 
 app.useRouter(router)
+app.useMiddleware(middleware)
 const port =  3000;
 app.start(port, () => {
     console.log(`Server running at http://localhost:${port}`);
